0030cf8daafbfcc227bf44758c6224da91930a42
Lin Zhu <linp9595@gmail.com>
2020-07-17 23:42:11 -0700

    Finished leetcode #27

---
+++ 27.remove-element.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=27 lang=cpp
+ *
+ * [27] Remove Element
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+       int count = 0;
+       for(int i = 0; i < nums.size(); ++i){
+           if(nums[i] == val){
+               count++;
+           }
+           else
+           {
+               nums[i-count] = nums[i];
+           }
+       } 
+       return nums.size()-count;
+    }
+};
+// @lc code=end
+
