a6796e08779dfa405ca9c041112b187abd616290
Lin Zhu <linp9595@gmail.com>
2020-07-19 23:58:17 -0700

    #14 and # 66

---
+++ 66.plus-one.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=66 lang=cpp
+ *
+ * [66] Plus One
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        for (int i = n - 1; i >= 0; --i)
+        {
+            if (digits[i] == 9)
+            {
+                digits[i] = 0;
+            }
+            else
+            {
+                digits[i]++;
+                return;
+            }
+        }
+        digits[0] = 1;
+        digits.push_back(0);
+    }
+};
+// @lc code=end
+
