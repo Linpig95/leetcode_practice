7c49c808b42267ed3686d313d40b2d6915ab1a30
Lin Zhu <linp9595@gmail.com>
2020-07-28 23:31:43 -0700

    Finished #110

--- 110.balanced-binary-tree.cpp
+++ 110.balanced-binary-tree.cpp
@@ -22,18 +22,20 @@ public:
     int depth(TreeNode *root)
     {
         if (root == NULL)
-        {
             return 0;
-        }
-        return max(depth(root->left), depth(root->right)) + 1;
+        int leftHeight = depth(root->left);
+        if (leftHeight == -1)
+            return -1;
+        int rightHeight = depth(root->right);
+        if (rightHeight == -1)
+            return -1;
+        if (abs(leftHeight - rightHeight) > 1)
+            return -1;
+        return max(leftHeight, rightHeight) + 1;
     }
     bool isBalanced(TreeNode *root)
     {
-        if (root == NULL)
-            return true;
-        int left = depth(root->left);
-        int right = depth(root->right);
-        return abs(left - right) <= 1 && isBalanced(root->left) && isBalanced(root->right);
+        return depth(root) != -1;
     }
 };
 // @lc code=end
