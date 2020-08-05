4a5764d660cc4d27b67e617324d724da6e72577c
Lin Zhu <linp9595@gmail.com>
2020-08-04 23:52:14 -0700

    Finished leetcode#242

---
+++ 242.valid-anagram.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=242 lang=cpp
+ *
+ * [242] Valid Anagram
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+    int alp[26]={};
+    for (int i = 0; i < s.length(); i++) 
+        alp[s.at(i) - 'a']++;
+    for (int i = 0; i < t.length(); i++)
+        alp[t.at(i) - 'a']--;
+    for (int i=0;i<26;i++)
+        if (alp[i] != 0) 
+            return false;
+        return true;
+   }
+};
+// @lc code=end
+
