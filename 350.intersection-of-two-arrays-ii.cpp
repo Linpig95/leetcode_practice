16f9122972b416b70c8ed6b9e94fa69a5d328e7c
Lin Zhu <linp9595@gmail.com>
2020-08-07 23:48:38 -0700

    Finished leetcode#350

---
+++ 350.intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=350 lang=cpp
+ *
+ * [350] Intersection of Two Arrays II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+       unordered_map<int, int> dict;
+        vector<int> res;
+        for(int i = 0; i < (int)nums1.size(); i++) dict[nums1[i]]++;
+        for(int i = 0; i < (int)nums2.size(); i++)
+            if(--dict[nums2[i]] >= 0) res.push_back(nums2[i]);
+        return res; 
+    }
+};
+// @lc code=end
+
